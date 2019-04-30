2cd2a545861cd2788fac6320de0f8114ca5250f9
ant4u <anthonyimontalto@gmail.com>
2019-04-30 16:00:59 -0400

    build

---
+++ client/build/precache-manifest.c3b10acd4d30054ccc8c8ae96dab5100.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d94b3d7583faa094e851",
+    "url": "/static/js/main.d94b3d75.chunk.js"
+  },
+  {
+    "revision": "a127860131b4c8ffc663",
+    "url": "/static/js/2.a1278601.chunk.js"
+  },
+  {
+    "revision": "d94b3d7583faa094e851",
+    "url": "/static/css/main.193f7293.chunk.css"
+  },
+  {
+    "revision": "a89429c9816022a724c1e97e57e2709e",
+    "url": "/index.html"
+  }
+];
