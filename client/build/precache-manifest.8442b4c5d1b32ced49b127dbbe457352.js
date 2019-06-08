866a40691bc0d8dae6dc09ff7af18315af2c018d
ant4u <anthonyimontalto@gmail.com>
2019-06-08 06:57:59 -0400

    updated package versions

---
+++ client/build/precache-manifest.8442b4c5d1b32ced49b127dbbe457352.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f998e3134a43ff70f2da4e675cd387b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3276705fda615275da33",
+    "url": "/static/css/main.5a4bee35.chunk.css"
+  },
+  {
+    "revision": "1abb0a84b87fd28243ec",
+    "url": "/static/js/2.3e05f33f.chunk.js"
+  },
+  {
+    "revision": "3276705fda615275da33",
+    "url": "/static/js/main.495fb442.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
